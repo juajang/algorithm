963ca17a67f621d85261928690f4042af9737ba3
JangJua <jua0610@naver.com>
2020-07-07 21:42:30 +0900

    청소년 상어

--- Simulation/청소년 상어.cpp
+++ Simulation/청소년 상어.cpp
@@ -11,24 +11,20 @@ struct Fish {
 int max(int a, int b) { return a > b ? a : b; }
 
 int dfs(int sum, Fish shark, vector<vector<int>> MAP, vector<Fish> fish) {
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			cout << MAP[i][j] << " " << fish[MAP[i][j]].dir << " ";
-		}
-		cout << "\n";
-	}
-	cout << "\n";
 
+	//상어가 현재 위치의 물고기 먹기 
 	shark.dir = fish[MAP[shark.x][shark.y]].dir;
 	sum += MAP[shark.x][shark.y];
+	fish[MAP[shark.x][shark.y]].dir = -1;
 	MAP[shark.x][shark.y] = 0;
-	fish[MAP[shark.x][shark.y]] = { 0, 0, 0 };
 
 	for (int i = 1; i <= 16; i++) {
+
+		//각각의 물고기 1번부터 이동 
 		int x = fish[i].x;
 		int y = fish[i].y;
 		int dir = fish[i].dir;
-		if (MAP[x][y] == 0) continue;  
+		if (dir == -1) continue; //이미 먹힌 물고기는 이동하지 않음 
 
 		for (int j = 0; j < 8; j++) {
 			int ndir = dir + j <= 8 ? dir + j : dir + j - 8;
@@ -36,42 +32,33 @@ int dfs(int sum, Fish shark, vector<vector<int>> MAP, vector<Fish> fish) {
 			int ny = y + direction[ndir][1];
 			if (shark.x == nx && shark.y == ny) continue;
 			if (nx < 0 || nx >= 4 || ny < 0 || ny >= 4) continue;
-		 
+
+			//다른 물고기와 swap
 			int next = MAP[nx][ny];
 			MAP[nx][ny] = MAP[x][y];
 			MAP[x][y] = next;
-			fish[i].x = nx; fish[i].y = ny; fish[i].dir = ndir; 
-			fish[next].x = x; fish[next].y = y; 
-			break;
+			fish[i].x = nx; fish[i].y = ny; fish[i].dir = ndir;
+			fish[next].x = x; fish[next].y = y;
+			break; //이동을 완료하면 그만 두기 
 		}
 	}
-
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			cout << MAP[i][j] << " " << fish[MAP[i][j]].dir << "| ";
-		}
-		cout << "\n";
-	}
-	cout << "\n";
-
-
+	
 	int max_sum = sum;
 	for (int i = 1; i <= 3; i++) {
 		int sdir = shark.dir;
 		int nx = shark.x + direction[sdir][0] * i;
 		int ny = shark.y + direction[sdir][1] * i;
-		printf("%d %d %d: %d %d + %d %d -> %d %d\n", i, sdir, sum, shark.x, shark.y, direction[sdir][0] * i, direction[sdir][1] * i, nx, ny);
 		if (nx < 0 || nx >= 4 || ny < 0 || ny >= 4) continue;
 		if (MAP[nx][ny] == 0) continue;
 		max_sum = max(dfs(sum, { nx, ny, sdir }, MAP, fish), max_sum);
 	}
-	
+
 	return max_sum;
 }
 
 int main() {
 	int no, dir;
-	vector<vector<int>> MAP(4, vector<int>(4,0));
+	vector<vector<int>> MAP(4, vector<int>(4, 0));
 	vector<Fish> fish(17);
 
 	for (int i = 0; i < 4; i++) {
