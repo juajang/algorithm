74e668e5b01290cb25cbd5f1a0eeb15ca0019971
JangJua <jua0610@naver.com>
2020-07-14 23:47:30 +0900

    주석 추가

--- DP/미로 탈출하기.cpp
+++ DP/미로 탈출하기.cpp
@@ -7,11 +7,13 @@ char maze[500][500];
 int cache[500][500];
 
 bool dfs(int x, int y) {
+	//미로를 탈출할 수 있는 경우 
 	if (x < 0 || x >= N || y < 0 || y >= M) return true;
 
 	int& ret = cache[x][y];
-	if (ret != -1) return ret;
+	if (ret != -1) return ret; //중복 탐색 방지 
 
+	//미로를 탐색하다가 결국 탐색 전 0으로 초기해놨었던 처음 출발 위치로 돌아오게 됐다는 것은 탈출이 불가능한 위치라는 것을 의미한다.  
 	ret = 0;
 	if (maze[x][y] == 'U') {
 		ret = dfs(x - 1, y);
