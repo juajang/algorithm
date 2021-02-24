220956afa6b197e6883e23354d73583e9a6ac3c8
JangJua <jua0610@naver.com>
2021-02-24 23:45:21 +0900

    2021/2/24 PS

---
+++ Etc/알파벳.py
@@ -0,0 +1,27 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+d = ((1, 0), (-1, 0), (0, 1), (0, -1))
+
+R, C = map(int, input().split())
+arr = [list(input()) for _ in range(R)]
+
+x, y = 0, 0
+queue = set()
+queue.add((x, y, arr[0][0]))
+answer = 1
+
+while queue:
+    x, y, alpha = queue.pop()
+    answer = max(answer, len(alpha))
+    for i in range(4):
+        nx = x + d[i][0]
+        ny = y + d[i][1]
+        if nx < 0 or nx >= R or ny < 0 or ny >= C:
+            continue
+        if arr[nx][ny] in alpha: 
+            continue
+        queue.add((nx, ny, alpha + arr[nx][ny]))
+
+print(answer)
