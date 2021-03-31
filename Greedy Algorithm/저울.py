dd6ded90f1e41ca6678c9e55a8fac2f5ff7e7a8f
juajang <jua.jang@kakao.com>
2021-03-31 23:56:53 +0900

    2021/03/31 PS

--- Greedy Algorithm/저울.py
+++ Greedy Algorithm/저울.py
@@ -2,12 +2,15 @@ N = int(input())
 w = list(map(int, input().split()))
 
 w.sort()
-total = 1
+answer = 0
+total = 0
 for weight in w:
-    if weight <= total:
+    if weight <= total + 1:
         total += weight
     else:
         break
 
-answer = total 
-print(answer)
+if total == 0:
+    print(1)
+else:
+    print(answer)
