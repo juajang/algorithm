1c49916b6dbee34c683fb3637806ec35dd21555b
juajang <jua.jang@kakao.com>
2021-04-15 23:53:10 +0900

    2021/04/15 PS

---
+++ Backtracking/사다리 조작.py
@@ -0,0 +1,41 @@
+from itertools import combinations
+import sys
+input = sys.stdin.readline
+
+N, M, H = map(int, input().split())
+ladders = [[0] * (H+1) for _ in range(N+1)]
+
+# ladders[b][a] b번 세로선과 b+1번 세로선을 a에서 연결 
+for _ in range(M):
+    a, b = map(int, input().split())
+    ladders[b][a] = 1
+
+def check():
+    for i in range(1, N+1):
+        curr = i
+        for j in range(1, H+1):
+            if ladders[curr][j]:
+                curr += 1
+            elif ladders[curr-1][j]:
+                curr -= 1
+        if curr != i:
+            return False
+    return True    
+
+def backtracking(count):
+    global answer
+    if count >= answer:
+        return
+    if check():
+        answer = count
+        return
+    for i in range(1, N):
+        for j in range(1, H+1):
+            if ladders[i][j] == 0 and ladders[i-1][j] == 0 and ladders[i+1][j] == 0:
+                ladders[i][j] = 1
+                backtracking(count + 1)
+                ladders[i][j] = 0
+
+answer = 4
+backtracking(0)
+print(answer if answer < 4 else -1)
